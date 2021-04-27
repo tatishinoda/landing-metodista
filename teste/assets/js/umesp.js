e3d80c1fb3d6e122574e418d5053710e6ceeb77d
Tatiane Gabrielle <shinoda_@hotmail.co.uk>
2021-04-27 10:33:16 -0300

    modelo landing 2º semestre

--- teste/assets/js/umesp.js
+++ teste/assets/js/umesp.js
@@ -33,21 +33,3 @@ $("[name=crmeduc_modalidade_interesse]").on("change", function () {
     });
     
 
-//Submit Form
-   // this is the id of the form
-   $("#form-estrangeiro").submit(function (e) {
-      e.preventDefault(); // avoid to execute the actual submit of the form.
-
-      var form = $(this);
-      var url = form.attr("action");
-
-      $.ajax({
-         type: "POST",
-         url: url,
-         data: form.serialize(), // serializes the form's elements.
-         success: function () {
-            alert("Dados Enviados com Sucesso"); // show response from the php script.
-            $('#form-estrangeiro')[0].reset();
-         }
-      });
-   });
